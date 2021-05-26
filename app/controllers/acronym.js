861fb18fa564ccf914917a74cc26ef8c82b42707
Uriel Ventura <urielventura000@gmail.com>
2021-05-26 00:57:27 -0400

    Update acronym.js
    Begin the Post new acronym

--- app/controllers/acronym.js
+++ app/controllers/acronym.js
@@ -2,8 +2,17 @@ const Db = require('../../acronym.json')
 
 exports.GetData = async( req, res) => {      
     try {
-        res.send({data: Db})
+        res.status(200).send({data: Db})
     } catch (error) {
         res.status(500).send('Something broke!');
+    }
+}
+
+
+exports.PostNewAcronym = async( req, res ) => {
+    try {
+        
+    } catch (error) {
+        
     }
 }
